9406161af5d29cc886210346cd0d8a0df1025ea0
Nils L. Westhausen <breizhn@users.noreply.github.com>
2020-06-24 21:49:49 +0200

    changed path to model

--- measure_execution_time.py
+++ measure_execution_time.py
@@ -22,7 +22,7 @@ os.environ["CUDA_VISIBLE_DEVICES"]=''
 
 if __name__ == '__main__':
     # loading model in saved model format
-    model = tf.saved_model.load('./dtln_saved_model')
+    model = tf.saved_model.load('./pretrained_model/dtln_saved_model')
     # mapping signature names to functions
     infer = model.signatures["serving_default"]
         
